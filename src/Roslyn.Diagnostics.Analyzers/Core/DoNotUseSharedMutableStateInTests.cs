6c01e7faa5611110a9d210e2ddb71f31daa36745
Jason Malinowski <jason.malinowski@microsoft.com>
2016-01-28 00:24:28 -0800

    WIP

--- src/Roslyn.Diagnostics.Analyzers/Core/DoNotUseSharedMutableStateInTests.cs
+++ src/Roslyn.Diagnostics.Analyzers/Core/DoNotUseSharedMutableStateInTests.cs
@@ -3,6 +3,7 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Roslyn.Diagnostics.Analyzers
 {
@@ -18,7 +19,7 @@ namespace Roslyn.Diagnostics.Analyzers
                                                                              s_localizableTitle,
                                                                              s_localizableMessage,
                                                                              DiagnosticCategory.Reliability,
-                                                                             DiagnosticSeverity.Error,
+                                                                             DiagnosticSeverity.Warning,
                                                                              isEnabledByDefault: true,
                                                                              helpLinkUri: null,     // TODO: add MSDN url
                                                                              customTags: WellKnownDiagnosticTags.Telemetry);
@@ -38,10 +39,63 @@ namespace Roslyn.Diagnostics.Analyzers
 
         private void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            if (context.Symbol.IsStatic)
+            var fieldSymbol = (IFieldSymbol)context.Symbol;
+
+            if (fieldSymbol.IsStatic && !fieldSymbol.IsConst)
             {
-                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Symbol.Locations.First(), context.Symbol.ToDisplayString()));
+                if (fieldSymbol.IsReadOnly)
+                {
+                    if (!TypeIsImmutableWhenReadOnly(fieldSymbol.Type))
+                    {
+                        // The field is read only but the underlying type isn't, so fail
+                        context.ReportDiagnostic(Diagnostic.Create(Rule, context.Symbol.Locations.First(), context.Symbol.ToDisplayString()));
+                    }
+                }
+                else
+                {
+                    // The field isn't read only, so most definitely not
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, context.Symbol.Locations.First(), context.Symbol.ToDisplayString()));
+                }
             }
+        }
+
+        private static readonly ImmutableHashSet<string> TypesThatAreImmutableWhenReadOnly;
+
+        static DoNotUseSharedMutableStateInTests()
+        {
+            var builder = ImmutableHashSet<string>.Empty.ToBuilder();
+
+            builder.Add("System.Int32");
+            builder.Add("System.Lazy`1");
+            builder.Add("System.TimeSpan");
+            builder.Add("System.String");
+            builder.Add("System.Collections.Immutable.ImmutableArray");
+            builder.Add("System.Text.RegularExpressions.Regex");
+
+            // A ComposableCatalog is just the metadata of discovered types. It is immutable.
+            // This is distinct from the ExportProvider which actually holds onto instances of the
+            // created parts.
+            builder.Add("Microsoft.VisualStudio.Composition.ComposableCatalog");
+
+            TypesThatAreImmutableWhenReadOnly = builder.ToImmutableHashSet();
+        }
+
+        private static bool TypeIsImmutableWhenReadOnly(ITypeSymbol type)
+        {
+            if (!TypesThatAreImmutableWhenReadOnly.Contains(type.ContainingNamespace.MetadataName + "." + type.MetadataName))
+            {
+                return false;
+            }
+
+            var namedTypeSymbol = type as INamedTypeSymbol;
+
+            if (!namedTypeSymbol.ConstructedFrom.Equals(namedTypeSymbol))
+            {
+                // The type is constructed, so we must check that it's constructed with immutable things too
+                return namedTypeSymbol.TypeParameters.All(TypeIsImmutableWhenReadOnly);
+            }
+
+            return true;
         }
     }
 }
